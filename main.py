f7aadc8c805d792b84edef01184ad8e18b56b5b8
xz454867105 <454867105@qq.com>
2021-12-09 15:29:48 +0800

    2.1.2

--- main.py
+++ main.py
@@ -1,29 +1,69 @@
-# -*- coding:utf-8 -*-
-import src.user.getUser as getUser
-import src.course.foreWork as foreWork
-import src.course.doWork as doWork
-# import src.show.mdshow as mdshow
-import src.config.config as config
+import requests
+from base64 import b64encode
+import json
+
+import src.log as log
 
 
-if __name__ == '__main__':
+class User:
+    def __init__(self, usernm, passwd):
+        self.usernm = str(usernm)
+        self.passwd = str(passwd)
+        self.logger = log.Logger("logs/{}.txt".format(usernm))
 
-    isallow,usernm,passwd,courseid,speed = config.get_params()
+    def verify(self):
+        url = 'https://passport2-api.chaoxing.com/fanyalogin'
+        headers = {
+            'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 5.1.1; SM-G9350 Build/LMY48Z) com.chaoxing.mobile/ChaoXingStudy_3_5.21_android_phone_206_1 (SM-G9350; Android 5.1.1; zh_CN)_19698145335.21',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+        data = {
+            'fid': '-1',
+            'uname': str(self.usernm),
+            'password': b64encode(self.passwd.encode('utf8')),
+            'refer': 'http%3A%2F%2Fi.mooc.chaoxing.com',
+            't': 'true',
+            'forbidotherlogin': '0',
+        }
+        self.logger.info("正在尝试登录账号: {}".format(self.usernm))
+        resp = requests.post(url, data=data)
+        try:
+            if resp.status_code == 200 and resp.json().get('status') == True:
+                self.logger.info("登录成功，账户: {}验证完毕".format(self.usernm))
+                return {'code':'1'}
+            else:
+                self.logger.error("登录失败，账户: {}账号或密码不正确".format(self.usernm))
+                return {'code':'-1'}
+        except json.decoder.JSONDecodeError as e:
+            self.logger.debug(resp.text)
+            self.logger.critical("登录失败，请求获取的结果非JSON，已在日志中输出请求结果")
+            self.logger.critical("请查看日志文件")
+            return {'code':'-2'}
 
-    # 展示md信息文件 默认为不显示
-    # mdshow.print_md(isallow)
+    def get_info(self):
+        pass
 
-    # 从本地或在线获取用户信息 默认为空
-    usernm, session = getUser.get_session(usernm,passwd)
-
-    # 从本地或在线获取课程信息以及各类任务点信息
-    session, jobs, course, mp4, ppt = foreWork.get_forework_done(usernm, session,courseid)
-
-    # 开始完成MP4任务点 速度默认为一倍速
-    doWork.do_mp4(usernm, course, session, mp4, int(speed))
-
-    # 开始完成PPT任务点
-    # doWork.do_ppt(session, mp4, ppt, usernm, course)
-    
-    # 提示任务全部结束
-    input("所有可识别的任务已经完成，请点击回车键退出程序")
+        # if resp.status_code == 200:
+        #     if resp.json()['status']:
+        #         console.log("[yellow]登录成功[/yellow]")
+        #         pathCheck.check_file('saves/{}/userinfo.json'.format(usernm))
+        #         cookie = requests.utils.dict_from_cookiejar(resp.cookies)
+        #         user['usernm'] = usernm
+        #         user['passwd'] = passwd
+        #         user['userid'] = cookie['_uid']
+        #         user['fid'] = cookie['fid']
+        #         console.log("正在[red]本地[/red]保存账户信息")
+        #         with open('saves/{}/userinfo.json'.format(usernm), 'w') as f:
+        #             json.dump(user, f)
+        #         console.log("[yellow]账户信息[/yellow]保存成功")
+        #     else:
+        #         console.input("[red]登录失败[/red],请检查你的[red]账号密码[/red]是否正确,按回车键退出")
+        #         # print('登录失败，请检查你的账号密码,按回车键退出')
+        #         exit()
+        # else:
+        #     console.log("登录失败，登录返回状态码[red]{}[/red]".format(resp.status_code))
+        #     console.log("返回内容：\n")
+        #     console.log(resp.text)
+        #     console.input("请在仓库Issue页面提出反馈，按回车键退出")
+        #     exit()
+        # return session
---
+++ src/log.py
@@ -0,0 +1,34 @@
+import logging
+
+
+class Logger:
+    def __init__(self, path, stream = True, output = True, slevel = logging.INFO, olevel = logging.DEBUG):
+        self.logger = logging.getLogger(path)
+        self.logger.setLevel(logging.DEBUG)
+        if stream:
+            # 设置CMD日志
+            sh = logging.StreamHandler()
+            sh.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s', '%Y-%m-%d %H:%M:%S'))
+            sh.setLevel(slevel)
+            self.logger.addHandler(sh)
+        if output:
+            # 设置文件日志
+            fh = logging.FileHandler(path)
+            fh.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] [file: %(filename)s] [func: %(funcName)s] [line: %(lineno)d] %(message)s', '%Y-%m-%d %H:%M:%S'))
+            fh.setLevel(olevel)
+            self.logger.addHandler(fh)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def war(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
